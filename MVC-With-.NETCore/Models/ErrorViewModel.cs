cd9aca1b3efb60fd330fdf223c2db99e4f5a7dd2
Zakeria Hirsi <hirsiz@aston.ac.uk>
2018-11-10 22:08:28 +0000

    Add project files.

---
+++ MVC-With-.NETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_With_.NETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
